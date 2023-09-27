991fe57ab2a866911a70a17d8e3f04ed6c97215f
Abaacus <theboss2643@gmail.com>
2023-09-27 10:48:25 -0400

    Added astronomy code, examples & docs; changes to run.py
    * Added astronomy code
    
    * Added astronomy example-related code
    
    * Added documentation for astronomy examples
    
    * Added galaxy_decompose method to run.py

--- examples/example_4/example_4.py
+++ examples/example_4/example_4.py
@@ -15,5 +15,30 @@
 # <https://www.gnu.org/licenses/>.                                                                                     #
 ########################################################################################################################
 
-from .galaxy import * 
-from .misc import * 
+## Section 1: importing modules
+
+import os
+from shapelets.astronomy import (
+    load_fits_data,
+    get_postage_stamps,
+    decompose_galaxies
+)
+
+## Section 2: parameters
+fits_path = "images/galaxies.fits"
+shapelet_order = 10
+compression_order = 20
+
+## Section 3: code
+
+# 3.1: loading .fits data and output directory handling
+save_path = os.getcwd()+'/output/'
+if not os.path.exists(save_path): os.mkdir("output")
+output_base_path = save_path+fits_path[fits_path.rfind('/'):-5]
+data = load_fits_data(fits_path)
+
+# 3.2 iidentifying areas in the image that contain decomposable galaxies
+(galaxy_stamps, star_stamps, noiseless_data) = get_postage_stamps(data, output_base_path)
+
+# 3.3 starting with the biggest galaxy, decomposes subdomain into a collection of shapelet coefficients
+decompose_galaxies(galaxy_stamps, star_stamps, noiseless_data, shapelet_order, compression_order, output_base_path)
