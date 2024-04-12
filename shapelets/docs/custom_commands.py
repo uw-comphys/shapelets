b538d937b0e8397830842c705e6854258c4a3444
Matthew Tino <mptino@uwaterloo.ca>
2024-04-12 17:43:47 -0400

    Support to include tests with package installation

--- shapelets/docs/custom_commands.py
+++ shapelets/docs/custom_commands.py
@@ -22,7 +22,7 @@ r"""
 The shapelets package makes use of [entry points](https://packaging.python.org/en/latest/specifications/entry-points/).
 These are custom command-line arguments that trigger specific programming tasks in the background.
 
-* `shapelets CONFIG` - here, `CONFIG` is the name of the text-based configuration file that will perform some specific analysis based on the parameters present in the file. See the package [examples](https://uw-comphys.github.io/shapelets/shapelets/docs.html).
-* `shapelets-test` - triggers all unit tests in the [tests](https://github.com/uw-comphys/shapelets/tree/main/tests) directory and will report any failures. It is encouraged to use this command if modifying the package source code and after initially [installing](https://uw-comphys.github.io/shapelets/shapelets/docs/installation_guide.html) the `shapelets` package.
+* `shapelets CONFIG` - this entry point is used to interact with the shapelets package via configuration files. Here `CONFIG` is the name of the text-based configuration file that will perform a particular analysis based on its contents. See the package [examples](https://uw-comphys.github.io/shapelets/shapelets/docs.html) for useful demonstration. Note that the configuration file must be in the same directory as your working (terminal) directory to use this entry point correctly.
+* `shapelets-test` - triggers all [unit tests](https://github.com/uw-comphys/shapelets/tree/main/shapelets/tests) and will report any failures. It is encouraged to use this command if (1) you are modifying the package source code and (2) after initial [installation](https://uw-comphys.github.io/shapelets/shapelets/docs/installation_guide.html) of the shapelets package.
 
 """
--- shapelets/tests/__init__.py
+++ shapelets/tests/__init__.py
@@ -15,50 +15,5 @@
 # <https://www.gnu.org/licenses/>.                                                                                     #
 ########################################################################################################################
 
-import os
-from pathlib import Path
-import platform
-import sys 
-import time
-
-from . import _run
-
-def _run_shapelets():
-    r"""
-    Main function that runs shapelets. This is only invoked via the entry point "shapelets CONFIG" where CONFIG is the name of the configuration plaintext file that exists in the same directory level as the working directory.  
-
-    """
-    # if user did not provide any configuration filename (which is required)
-    if len(sys.argv) == 1: 
-        raise RuntimeError('Please provide name of config file, i.e. "shapelets config".')
-
-     # if user did provide a configuration filename/path
-    elif len(sys.argv) == 2:
-        config_file = sys.argv[1]
-        working_dir = os.getcwd() 
-        _run._run(config_file, working_dir)
-
-    # if the user provides more than 1 argument (in addition to shapelets). Print error messages and quit.
-    else: 
-        raise RuntimeError('Please provide one argument (configuration filename), i.e.: "shapelets config".')
-
-
-def _run_tests():
-    r"""
-    Main function that runs all the unit tests via unittest from Python STL. This is only invoked via the entry point "shapelets-test".
-    
-    """
-    # notify user tests may take more than a few seconds
-    print("Initiating shapelets unit tests. This will likely take a few minutes.")
-    time.sleep(10)
-    
-    # navigate to tests/ folder relative to this file
-    tests_dir = os.path.join(Path(__file__).parents[0], 'tests')
-    os.chdir(tests_dir)
-
-    # command line arguments based on user OS
-    # automatically find all tests using unittest built-in discovery component
-    if str(platform.system()) == 'Windows':
-        os.system('python -B -m unittest -v')
-    else:
-        os.system('python3 -B -m unittest -v')               
+# Define __all__ as empty list such that tests are not rendered on pdoc website documentation
+__all__ = []
