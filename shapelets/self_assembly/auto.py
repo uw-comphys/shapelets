7860422a7e2eb2dd661a79e17e8ec6fdf187bd83
Cameron Chin <105403740+cjcrjc@users.noreply.github.com>
2025-02-08 18:40:21 -0500

    MNT: changed variables names and relative pathing

--- shapelets/self_assembly/auto.py
+++ shapelets/self_assembly/auto.py
@@ -33,14 +33,14 @@ from .quant import rdistance
 from .wavelength import get_wavelength
 
 
-def infer(RVs: np.ndarray) -> str:
+def infer(response_vectors: np.ndarray) -> str:
     r"""
     This function performs pattern inference using a trained CNN model to predict the pattern present in image.
     The shapelets response vectors are otsu thresholded and resized to 256x256 prior to inference.
 
     Parameters
     ----------
-    * RVs: np.ndarray
+    * response_vectors: np.ndarray
         Response vectors of channels m = [2, 3, 6].
 
     Returns
@@ -53,27 +53,29 @@ def infer(RVs: np.ndarray) -> str:
     TODO: Need to reference paper.
 
     """
-    if not isinstance(RVs, np.ndarray):
-        raise ValueError("RVs must be a numpy array")
-    if RVs.shape[2] != 3:
-        raise ValueError("RVs must have 3 channels")
-    if np.min(RVs) < 0 or np.max(RVs) > 1:
-        raise ValueError("RVs must be normalized to [0, 1]")
+    if not isinstance(response_vectors, np.ndarray):
+        raise ValueError("response_vectors must be a numpy array")
+    if response_vectors.shape[2] != 3:
+        raise ValueError("response_vectors must have 3 channels")
+    if np.min(response_vectors) < 0 or np.max(response_vectors) > 1:
+        raise ValueError("response_vectors must be normalized to [0, 1]")
 
     # Load the best model from train.py
-    model_path = r"./self_assembly/models/CNN.h5"
+    model_path = Path(__file__).parent / "models" / "CNN.h5"
     model = tf.keras.models.load_model(model_path)
 
     # Apply Otsu thresholding to each channel
     for i in range(3):
-        threshold = threshold_otsu(RVs[:, :, i])
-        RVs[:, :, i] = np.where(RVs[:, :, i] > threshold, RVs[:, :, i], 0)
+        threshold = threshold_otsu(response_vectors[:, :, i])
+        response_vectors[:, :, i] = np.where(response_vectors[:, :, i] > threshold, response_vectors[:, :, i], 0)
 
     # Resize to 256x256 and convert to float32
-    RVs = np.expand_dims(np.array(Image.fromarray((RVs * 255).astype(np.uint8)).resize((256, 256))), axis=0).astype(np.float32)
+    response_vectors = np.expand_dims(np.array(Image.fromarray((response_vectors * 255).astype(np.uint8)).resize((256, 256))), axis=0).astype(
+        np.float32
+    )
 
     # Predict the class index
-    predicted_class_index = model(RVs)
+    predicted_class_index = model(response_vectors)
     predicted_class_index = np.argmax(predicted_class_index, axis=1)
 
     # Convert index to class name
@@ -87,13 +89,13 @@ def infer(RVs: np.ndarray) -> str:
     return predicted_class
 
 
-def find_reference_region(img, threshold: int = -1, stride: int = 1):
+def find_reference_region(image: np.ndarray, threshold: int = -1, stride: int = 1):
     r"""
     This function performs a convolution operation on the input image to find an ideal reference region based on the minimum sum of the output of several convolutions.
 
     Parameters
     ----------
-    * img: np.ndarray
+    * image: np.ndarray
         Input image
     * threshold : int, optional
         If non-zero number, return information for all possible tiles after applying the stride and taking that top % of the ideal boxes. Default is -1 so no thresholding applied, only returns best box
@@ -114,26 +116,26 @@ def find_reference_region(img, threshold: int = -1, stride: int = 1):
     TODO: Need to reference paper.
 
     """
-    if not isinstance(img, np.ndarray):
-        raise ValueError("img must be a numpy array")
-    if img.ndim != 2:
-        raise ValueError("img must be a 2D array")
-    if np.min(img) < 0 or np.max(img) > 1:
-        raise ValueError("img must be normalized to [0, 1]")
+    if not isinstance(image, np.ndarray):
+        raise ValueError("image must be a numpy array")
+    if image.ndim != 2:
+        raise ValueError("image must be a 2D array")
+    if np.min(image) < 0 or np.max(image) > 1:
+        raise ValueError("image must be normalized to [0, 1]")
     if not (threshold == -1 or (threshold > 0 and threshold < 100)):
         raise ValueError("threshold must be -1 or a number between 0 and 100")
     if not isinstance(stride, int) or stride < 1:
         raise ValueError("stride must be a positive integer")
 
     # Window size set to 2.5x, where x is characteristic wavelength
-    char_wavelength = get_wavelength(image=img, verbose=False)
+    char_wavelength = get_wavelength(image=image, verbose=False)
     window_size = int(char_wavelength * 2.5)
 
     kernel = np.ones((window_size, window_size), dtype=np.float32)
-    convolved_img = fftconvolve(img.astype(np.float32), kernel, mode="valid")
+    convolved_image = fftconvolve(image.astype(np.float32), kernel, mode="valid")
 
     # Find the location of the minimum sum in the convolved image down to the pixel before stride
-    min_sum_idx = np.unravel_index(np.argmin(convolved_img), convolved_img.shape)
+    min_sum_idx = np.unravel_index(np.argmin(convolved_image), convolved_image.shape)
 
     # Calculate the coordinates of the ideal box based on the minimum sum location and stride
     x_start, y_start = min_sum_idx[1], min_sum_idx[0]
@@ -142,16 +144,16 @@ def find_reference_region(img, threshold: int = -1, stride: int = 1):
 
     # Take only stride-separated boxes
     if stride != 1:
-        convolved_img = convolved_img[::stride, ::stride]
+        convolved_image = convolved_image[::stride, ::stride]
 
     # If multiple regions are to be returned (thresholding mode)
     if threshold != -1:
         # Generate an array of top-left coordinates and their respective sums for each kernel position after stride is applied
         top_left_coords = []
         box_sums = []
-        for i in range(convolved_img.shape[0]):
-            for j in range(convolved_img.shape[1]):
-                box_sum = np.sum(img[i * stride : i * stride + window_size, j * stride : j * stride + window_size])
+        for i in range(convolved_image.shape[0]):
+            for j in range(convolved_image.shape[1]):
+                box_sum = np.sum(image[i * stride : i * stride + window_size, j * stride : j * stride + window_size])
                 box_sums.append(box_sum)
                 top_left_coords.append((j * stride, i * stride))
 
@@ -172,7 +174,7 @@ def find_reference_region(img, threshold: int = -1, stride: int = 1):
         return best_box
 
 
-def predict(pattern_type: str, RVs: np.ndarray, model_path: str):
+def predict(pattern_type: str, response_vectors: np.ndarray, model_path: str):
     r"""
     This function loads a trained MLPRegressor model based on the pattern type and predicts the response distance at each pixel.
     This generates a response distance image that can be used to approximate the response distance method.
@@ -181,7 +183,7 @@ def predict(pattern_type: str, RVs: np.ndarray, model_path: str):
     ----------
     * pattern_type : str
         Type of pattern ('hex' or 'stripe') for selecting the appropriate trained model
-    * RVs : np.ndarray
+    * response_vectors : np.ndarray
         Response vectors
     * model_path : str
         Directory path for the CNN prediction model
@@ -204,23 +206,20 @@ def predict(pattern_type: str, RVs: np.ndarray, model_path: str):
     """
     if not (pattern_type in ["hex", "stripe"]):
         raise ValueError("pattern_type must be 'hex' or 'stripe'")
-    if not isinstance(RVs, np.ndarray):
-        raise ValueError("RVs must be a numpy array")
-    if RVs.shape[2] != 29:
-        raise ValueError("RVs must have 29 channels")
+    if not isinstance(response_vectors, np.ndarray):
+        raise ValueError("response_vectors must be a numpy array")
+    if response_vectors.shape[2] != 29:
+        raise ValueError("response_vectors must have 29 channels")
     if not isinstance(model_path, str):
         raise ValueError("model_path must be a string")
     if not os.path.exists(model_path):
         raise ValueError("model_path must be a valid file path")
 
-    if pattern_type in ["hex", "stripe"]:
-        with open(model_path, "rb") as f:
-            regressor = Unpickler(f).load()
-    else:
-        raise ValueError('pattern_type parameter should be either "hex" or "stripe".')
+    with open(model_path, "rb") as f:
+        regressor = Unpickler(f).load()
 
     # Predict response distance at each pixel then reshape all estimated pixels to image shape
-    prediction = regressor.predict(RVs.reshape(-1, 29)).reshape(RVs.shape[:2])
+    prediction = regressor.predict(response_vectors.reshape(-1, 29)).reshape(response_vectors.shape[:2])
 
     return prediction
 
@@ -265,10 +264,10 @@ if __name__ == "__main__":
 
         # Compute 29th order shapelets and flatten to pass each pixel through regressor
         shapelet_order = 29
-        RVs = convresponse_n0(image=image, shapelet_order=shapelet_order, verbose=False)[0]
+        response_vectors = convresponse_n0(image=image, shapelet_order=shapelet_order, verbose=False)[0]
 
         # Get predicted pattern type from CNN inference
-        pattern_type = infer(RVs[:, :, [1, 2, 5]])
+        pattern_type = infer(response_vectors[:, :, [1, 2, 5]])
         print(f"Pattern type: {pattern_type}")
 
         # check pattern type
@@ -297,8 +296,8 @@ if __name__ == "__main__":
             # Split the selected filepath into the filename+ext and the rest of the path
             dir_path, filename = os.path.split(file_path)
             # Use predicted pattern type and filepath to get predicted response distance and ideal reference region location
-            model_path = os.path.join(Path(__file__).parents[0], f"MLPRegressor-{pattern_type}-denoising-std0.05.pickle")
-            prediction = predict(pattern_type, RVs, model_path)
+            model_path = Path(__file__).parent / "models" / f"MLPRegressor-{pattern_type}-denoising-std0.05.pickle"
+            prediction = predict(pattern_type, response_vectors, model_path)
             pred_time = time() - starttime
             print(f"Pred Time: {pred_time}")
             pred_times.append(pred_time)
@@ -343,15 +342,15 @@ if __name__ == "__main__":
 
             # TODO: should probably put this in a function to process output from this method
 
-            imgs = [image, prediction, rdist]
+            images = [image, prediction, rdist]
             lamb = int(get_wavelength(image, verbose=False))
             window_size = int(2.5 * lamb)
             names = [f"Lambda-{lamb}\npredicted-{pattern_type}", f"MLP_RD", "Kmeans_RD"]
 
             fig, axes = plt.subplots(1, 3)
-            for i in range(len(imgs)):
+            for i in range(len(images)):
                 # Plot each image in a subplot
-                axes[i].imshow(1 - imgs[i], cmap="gray")
+                axes[i].imshow(1 - images[i], cmap="gray")
                 axes[i].set_title(names[i])
                 # axes[i].axis('off')  # Turn off axis
                 axes[i].add_patch(Rectangle(top_left, width=window_size, height=window_size, linewidth=1, edgecolor="g", facecolor="none"))
